cf4ed21aaaf3c2bdff8260dca7d5ba6062dc8f0d
m-ello <sarateanualex4@gmail.com>
2025-05-10 20:03:13 +0300

    added -go fullscreen, -exit fullscreen and -quit

--- Software_QA/YouTubeTest.cs
+++ Software_QA/YouTubeTest.cs
@@ -19,12 +19,31 @@ namespace Software_QA
             driver.Navigate().GoToUrl("https://www.youtube.com");
             driver.Manage().Window.Maximize();
             Thread.Sleep(1000);
+            
+            //Accept cookies
             driver.FindElement(By.XPath("//*[@id=\"content\"]/div[2]/div[6]/div[1]/ytd-button-renderer[2]/yt-button-shape/button")).Click();
             Thread.Sleep(1000);
+            
+            //start search query
             driver.FindElement(By.Name("search_query")).SendKeys("behind blue eyes");
             driver.FindElement(By.XPath("//button[@aria-label='Search' and @title='Search']")).Click();
             Thread.Sleep(1000);
+
+            //find the video with solicited title
             driver.FindElement(By.XPath("//a[@id='video-title' and @title=concat('Limp Bizkit ', '\"', 'Behind Blue Eyes', '\"')]")).Click();
+            Thread.Sleep(3000);
+
+            //go fullscreen
+            //driver.FindElement(By.CssSelector("button.ytp-fullscreen-button.ytp-button")).Click();
+            new Actions(driver).SendKeys("f").Perform();
+            Thread.Sleep(10000);
+
+            //exit fullscreen
+            //driver.FindElement(By.CssSelector("button.ytp-fullscreen-button.ytp-button")).Click();
+            new Actions(driver).SendKeys("f").Perform();
+
+            //close the browser
+            driver.Quit();
         }
     }
 }
